7258b744512099c091b06c7f762dbbe484b543ff
kicap1992 <kicap.karan92@gmail.com>
2022-04-03 06:28:01 +0800

    added profile

--- frontend/components/halaman/Penulis.js
+++ frontend/components/halaman/Penulis.js
@@ -10,31 +10,31 @@ function Penulis(){
             <td>  </td>
             <td width="25%"> Nama </td>
             <td> <i>:</i> </td>
-            <td> <b> Namanya</b> </td>
+            <td> <b> Gusniarni Darwis</b> </td>
           </tr>
           <tr>
             <td>  </td>
             <td> NIM </td>
             <td> <i>:</i> </td>
-            <td> <b> Namanya</b> </td>
+            <td> <b> 217 280 213</b> </td>
           </tr>
           <tr>
             <td>  </td>
             <td> Pembimbing 1 </td>
             <td> <i>:</i> </td>
-            <td> <b> Namanya</b> </td>
+            <td> <b> Ir.Syahirun Alam, S.T ., M.T ., IPM</b> </td>
           </tr>
           <tr>
             <td>  </td>
             <td> Pembimbing 2 </td>
             <td> <i>:</i> </td>
-            <td> <b> Namanya</b> </td>
+            <td> <b> Ir.Untung Suwardoyo , S.Kom ., M.T ., IPP</b> </td>
           </tr>
           <tr>
             <td>  </td>
             <td> Judul </td>
             <td> <i>:</i> </td>
-            <td> <b> Namanya</b> </td>
+            <td className={classess.ini}> <b> IMPLEMENTASI <i>ARTIFICIAL INTELLIGENCE</i> MENGGUNAKAN LOGIKA <i>FUZZY</i> PADA TINGKAT KEMATANGAN BUAH DURIAN BERBASIS <i>WEB</i></b> </td>
           </tr>
         </table>
       </div>
